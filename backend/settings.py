d9fad6f5f75028e04669b105a58d9813a89dbb95
Zaid <karaymehzaid@gmail.com>
2022-03-08 08:02:55 +0300

    0.2.6 Beta Testing

--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,13 +80,27 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "d1mjbgmo2m0k4c",
+        'USER': "lzrlpvngolqdrl",
+        'PASSWORD': "b32d57b9a4c042ebbd56c3960da3f37f2199274acf213bcc916598403d4bff03",
+        'HOST': "ec2-34-247-151-118.eu-west-1.compute.amazonaws.com",
+        'PORT': '5432'
     }
-}
 
+    #     'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -140,3 +155,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = 'media/'
 
 TIME_ZONE = "Asia/Bahrain"
+
+django_heroku.settings(locals())
